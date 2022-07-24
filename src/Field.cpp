ad98a3ffe3193ba481439ec42f8643afd10aba31
yamashi <yamashi@users.noreply.github.com>
2022-07-24 17:38:15 +0200

    Add more types

--- src/Field.cpp
+++ src/Field.cpp
@@ -3,7 +3,7 @@
 template<>
 std::optional<uint64_t> Async::Field::As<uint64_t>() const noexcept
 {
-	try 
+	try
 	{
 		return std::stoull(m_data);
 	}
@@ -14,11 +14,63 @@ std::optional<uint64_t> Async::Field::As<uint64_t>() const noexcept
 }
 
 template<>
+std::optional<int64_t> Async::Field::As<int64_t>() const noexcept
+{
+	try
+	{
+		return std::stoll(m_data);
+	}
+	catch (...)
+	{
+		return std::nullopt;
+	}
+}
+
+template<>
+std::optional<uint32_t> Async::Field::As<uint32_t>() const noexcept
+{
+	try
+	{
+		return std::stoul(m_data);
+	}
+	catch (...)
+	{
+		return std::nullopt;
+	}
+}
+
+template<>
+std::optional<int32_t> Async::Field::As<int32_t>() const noexcept
+{
+	try
+	{
+		return std::stol(m_data);
+	}
+	catch (...)
+	{
+		return std::nullopt;
+	}
+}
+
+template<>
 std::optional<double> Async::Field::As<double>() const noexcept
 {
-	try 
+	try
 	{
 		return std::stod(m_data);
+	}
+	catch (...)
+	{
+		return std::nullopt;
+	}
+}
+
+template<>
+std::optional<float> Async::Field::As<float>() const noexcept
+{
+	try
+	{
+		return std::stof(m_data);
 	}
 	catch (...)
 	{
--- src/Field.hpp
+++ src/Field.hpp
@@ -18,9 +18,10 @@ namespace Async
 }
 
 extern template std::optional<uint64_t> Async::Field::As<uint64_t>() const noexcept;
-
+extern template std::optional<int64_t> Async::Field::As<int64_t>() const noexcept;
+extern template std::optional<uint32_t> Async::Field::As<uint32_t>() const noexcept;
+extern template std::optional<int32_t> Async::Field::As<int32_t>() const noexcept;
 extern template std::optional<double> Async::Field::As<double>() const noexcept;
-
+extern template std::optional<float> Async::Field::As<float>() const noexcept;
 extern template std::optional<std::string> Async::Field::As<std::string>() const noexcept;
-
 extern template std::optional<bool> Async::Field::As<bool>() const noexcept;
